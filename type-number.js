292dc82749dff253b020043e7cb0377ae88ea0c2
Seu Crush <victor.tsutsumi@ufu.com>
2021-08-09 09:33:33 -0300

    third commit

---
+++ type-number.js
@@ -0,0 +1,26 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = Math.PI;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(pontoFlutuanteSemZero);
+
+//NaN -> Not a Number (não é um número)
+
+const alura = "Alura e Lucas";
+
+console.log(alura);
+
